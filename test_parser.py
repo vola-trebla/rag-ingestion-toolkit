711b1eec6608869270ebbaa976927711b189c25d
Albert <alal@simpleswap.io>
2026-03-07 21:24:31 -0300

    feat: add PDF parser functionality

--- test_parser.py
+++ test_parser.py
@@ -1,8 +1,12 @@
 from parsers.html_parser import process_html_file
 from parsers.md_parser import process_markdown_file
+from parsers.pdf_parser import process_pdf_file
 
 result = process_html_file("input/test.html")
 print(result)
 
 result_md = process_markdown_file("input/test.md")
 print(result_md)
+
+result_pdf = process_pdf_file("input/test.pdf")
+print(result_pdf)
